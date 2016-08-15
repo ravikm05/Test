8f6315f1914d9cc8c7e5f1850c08ad20246ccbee
ravi-mrk <ravikiranm.35@gmail.com>
2016-08-15 07:55:03 +0000

    t

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello Jenkins Ji');
+  res.send('Hello Jenkins');
 });
  
 app.listen(process.env.PORT || 5000);
